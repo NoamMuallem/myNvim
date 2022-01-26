eae2427fdb3c55c689774535f1ea6d3def439600
NoamMuallem <noammu072@gmail.com>
2022-01-26 15:02:34 +0200

    updated dashboard with my name

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -28,7 +28,7 @@ local function footer()
 	-- local fortune = handle:read("*a")
 	-- handle:close()
 	-- return fortune
-	return "chrisatmachine.com"
+	return "Noam Muallem"
 end
 
 dashboard.section.footer.val = footer()
